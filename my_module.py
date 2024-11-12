b1fae711a67bfb53b9b7ce60db4100521ee33ffd
Logan Huffman <huffmanl@sou.edu>
2024-11-12 14:20:29 -0800

    Inclass extras

---
+++ my_module.py
@@ -0,0 +1,4 @@
+def greeting():
+    print('Hello, World!')
+    
+student1 = { 'name': 'Logan', 'age': 18 , 'hometown': 'Medford'}
---
+++ scratch.py
@@ -0,0 +1,22 @@
+#from random import randint
+#import random
+
+#print(random.randint(1, 5))
+
+#import my_module
+
+#print(dir(my_module))
+
+#my_module.greeting()
+
+#import pandas as pd 
+#import numpy as np
+
+#df = pd.DataFrame(np.random.randn(10, 5))
+#print(df)
+
+#from my_module import student1
+#import datetime
+
+#print(student1['age'])
+#print(datetime.datetime.now())
